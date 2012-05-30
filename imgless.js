d07777b0067b386e66f1775cdfec4803e1e5b11e
Michael <michael@donohoe.io>
2012-05-30 14:42:39 -0400

    Added frames and background-images to list

--- imgless.js
+++ imgless.js
@@ -1,9 +1,16 @@
+document.body.innerHTML += "<style>* {background-image: none !important;}</style>";
+
 var i = document.querySelectorAll("img");
 var o = document.querySelectorAll("object");
+var f = document.querySelectorAll("iframe");
+
 for (var a=0;a<i.length;a++) {
  i[a].src = "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==";
 }
 for (var b=0;b<o.length;b++) {
  o[b].data="";
  o[b].innerHTML="";
+}
+for (var c=0;c<f.length;c++) {
+ f[c].src="";
 }
