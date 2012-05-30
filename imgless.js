733e3d38fd900483b6a0935ad368c7128cfbee1e
Michael <michael@donohoe.io>
2012-05-30 14:49:17 -0400

    Delete embeds

--- imgless.js
+++ imgless.js
@@ -3,6 +3,7 @@ document.body.innerHTML += "<style>* {background-image: none !important;}</style
 var i = document.querySelectorAll("img");
 var o = document.querySelectorAll("object");
 var f = document.querySelectorAll("iframe");
+var e = document.querySelectorAll("embed");
 
 for (var a=0;a<i.length;a++) {
  i[a].src = "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==";
@@ -14,3 +15,7 @@ for (var b=0;b<o.length;b++) {
 for (var c=0;c<f.length;c++) {
  f[c].src="";
 }
+for (var d=0;d<e.length;d++) {
+ e[d].parentNode.removeChild(e[d]);
+}
+
